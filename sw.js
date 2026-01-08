5910a5b870b4e685f9a40baa66714540b89d804f
Edosgames <edomanini00@gmail.com>
2026-01-08 23:49:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,23 +2,22 @@ self.addEventListener("install", event => {
     event.waitUntil(
         caches.open("chess-cache").then(cache => {
             return cache.addAll([
-                "/",
-                "/index.html",
-                "/style.css",
-                "/app.js",
-                "/manifest.json",
-                "/pieces/bR.png",
-                "/pieces/bN.png",
-                "/pieces/bB.png",
-                "/pieces/bQ.png",
-                "/pieces/bK.png",
-                "/pieces/bP.png",
-                "/pieces/wR.png",
-                "/pieces/wN.png",
-                "/pieces/wB.png",
-                "/pieces/wQ.png",
-                "/pieces/wK.png",
-                "/pieces/wP.png"
+                "index.html",
+                "style.css",
+                "app.js",
+                "manifest.json",
+                "pieces/bR.png",
+                "pieces/bN.png",
+                "pieces/bB.png",
+                "pieces/bQ.png",
+                "pieces/bK.png",
+                "pieces/bP.png",
+                "pieces/wR.png",
+                "pieces/wN.png",
+                "pieces/wB.png",
+                "pieces/wQ.png",
+                "pieces/wK.png",
+                "pieces/wP.png"
             ]);
         })
     );
@@ -32,3 +31,4 @@ self.addEventListener("fetch", event => {
     );
 
 });
+
