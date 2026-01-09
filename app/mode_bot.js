c84b61fb2b118962df92b7a94aebe96e0eeb5c10
Edosgames <edomanini00@gmail.com>
2026-01-09 23:18:41 +0100

    Update mode_bot.js

--- app/mode_bot.js
+++ app/mode_bot.js
@@ -1,5 +1,30 @@
-// mode_classic.js
-function startGameMode_classic() {
+import { resetBoard, turn, getLegalMoves, applyMove } from "../core.js";
+import { fixCanvasResolution, updateSquareSize, drawBoard } from "../ui.js";
+import { enableInput } from "../input.js";
+
+export function startMode() {
     resetBoard();
-    drawBoard();
+    fixCanvasResolution();
+    updateSquareSize();
+    enableInput();
+    drawBoard(null, [], false, null, 0, 0);
+
+    document.getElementById("turnIndicator").textContent = "Tocca al Bianco";
+
+    window.onPlayerMove = () => {
+        if (turn === "b") {
+            const moves = [];
+
+            for (let y = 0; y < 8; y++)
+                for (let x = 0; x < 8; x++)
+                    for (let m of getLegalMoves(x, y))
+                        moves.push({ x1: x, y1: y, x2: m.x, y2: m.y });
+
+            if (moves.length > 0) {
+                const m = moves[Math.floor(Math.random() * moves.length)];
+                applyMove(m.x1, m.y1, m.x2, m.y2);
+                drawBoard(null, [], false, null, 0, 0);
+            }
+        }
+    };
 }
