97872dbd848118a6548000123e91103ce0b80334
Edosgames <edomanini00@gmail.com>
2026-01-09 23:35:49 +0100

    Update mode_classic.js

--- app/modes/mode_classic.js
+++ app/modes/mode_classic.js
@@ -2,12 +2,15 @@ import { resetBoard } from "../core.js";
 import { fixCanvasResolution, updateSquareSize, drawBoard } from "../ui.js";
 import { enableInput } from "../input.js";
 
-export function startMode() {
+export function startMode() {    
+    console.log("Modalità CLASSIC avviata");
+
     resetBoard();
+    initUI();              // <— IMPORTANTISSIMO
     fixCanvasResolution();
     updateSquareSize();
+
     enableInput();
     drawBoard(null, [], false, null, 0, 0);
-
     document.getElementById("turnIndicator").textContent = "Tocca al Bianco";
 }
