7bb0149afcc5f5171ba3ef0641a45f73dc502fea
Edosgames <edomanini00@gmail.com>
2026-01-10 02:35:21 +0100

    Update ui.js

--- app/ui.js
+++ app/ui.js
@@ -32,6 +32,19 @@ export function updateSquareSize() {
 
 export let size = 0;
 
+// Stato iniziale della scacchiera
+let board = [
+    ["r","n","b","q","k","b","n","r"],
+    ["p","p","p","p","p","p","p","p"],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["P","P","P","P","P","P","P","P"],
+    ["R","N","B","Q","K","B","N","R"]
+];
+
+
 // Cache immagini
 const pieces = {
     "r": "pieces/bR.png",
