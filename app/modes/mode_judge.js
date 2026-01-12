7cbe608d98d45582d54e26a1e32bb7513a7a569d
Edosgames <edomanini00@gmail.com>
2026-01-12 16:08:58 +0100

    Update mode_judge.js

--- app/modes/mode_judge.js
+++ app/modes/mode_judge.js
@@ -8,7 +8,18 @@ export function startMode() {
 
   resetBoard();
   // Inserisci Sentinella
-  board[3][3] = "S"; // esempio posizione
+  // Scegli una casella casuale tra le quattro centrali e inserisci la Sentinella
+  const centers = [
+    { x: 3, y: 3 }, // d4 (in notazione classica)
+    { x: 4, y: 3 }, // e4
+    { x: 3, y: 4 }, // d5
+    { x: 4, y: 4 }  // e5
+  ];
+  const pick = centers[Math.floor(Math.random() * centers.length)];
+
+  // Inserisci la Sentinella. Usiamo "S" (ma funziona anche "s": UI mappa entrambe).
+  board[pick.y][pick.x] = "S";
+  alert(`Sentinella posizionata in: x=${pick.x}, y=${pick.y}`);
 
   initUI();
 
