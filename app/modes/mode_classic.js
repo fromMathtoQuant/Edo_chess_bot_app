e6be054808cae83bbd150d5301a347e6ff9b365b
Edosgames <edomanini00@gmail.com>
2026-01-14 14:34:32 +0100

    Update mode_classic.js

--- app/modes/mode_classic.js
+++ app/modes/mode_classic.js
@@ -2,9 +2,14 @@ import { resetBoard } from "../core.js";
 import { fixCanvasResolution, updateSquareSize, drawBoard, initUI, preloadImages, updateTurnIndicator } from "../ui.js";
 import { enableInput } from "../input.js";
 
+
+
 export function startMode() {    
     console.log("Modalità CLASSIC avviata");
-        
+
+    // Disattiva il bot per questa modalità
+    window.onPlayerMove = null;
+    
     resetBoard();
     initUI();
 
